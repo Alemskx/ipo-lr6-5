0b93a82b80ae2e44396880d3e5b7cf3a97d1ea80
Alemskx <alemskx@gmail.com>
2024-11-05 23:54:43 +0300

    задание 5

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+#создание списка от -50 до 50
+a = [ random.randint(-50,50) for i in range(25)]
+print(a)
+print("Минимальное значение",min(a))
+print("Максимальное значение",max(a))
+count_positive = 0
+count_negative = 0
+count_zero = 0
+procent_zero = 0
+for i in a:
+    if i>0:
+        count_positive +=1
+        procent_positive = count_positive/25 * 100
+    if i<0:
+        count_negative +=1
+        procent_negative = count_negative/25 * 100
+    if i==0:
+        count_zero +=1
+        procent_zero = count_zero/25 * 100
+print("Количество положительных элементов: ",count_positive,"Их процент составляет: ",procent_positive)
+print("Количество отрицательных элементов: ",count_negative,"Их процент составляет:",procent_negative)
+print("Количество нулевых элементов: ",count_zero,"Их процент составляет: ",procent_zero)
